1abace76336186d4a69ace268a65f35d2fc3fed9
silmarasilva <silmara.eliza@gmail.com>
2024-05-24 19:58:41 -0300

    feat: variáveis

--- 1-Pacotes/auxiliar/auxiliar2.go
+++
@@ -1,7 +0,0 @@
-package auxiliar
-
-import "fmt"
-
-func Escrever2() {
-	fmt.Println("Escrevendo pela funcão escrever2")
-}
--- 1-Pacotes/go.mod
+++
@@ -1,5 +0,0 @@
-module modulo
-
-go 1.18
-
-require github.com/badoux/checkmail v1.2.4 // indirect
